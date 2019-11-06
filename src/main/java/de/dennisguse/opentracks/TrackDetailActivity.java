OpenTracksApp/OpenTracks

commit 353c958ae060d9306355921efae8cbb235c87bb5

Moved accessing sharedPreferences into PreferencesUtils.

src/main/java/de/dennisguse/opentracks/TrackDetailActivity.java
@@ -125,15 +125,15 @@ public class TrackDetailActivity extends AbstractListActivity implements ChooseA
             sharedPreferenceChangeListener = new OnSharedPreferenceChangeListener() {
         @Override
         public void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
-            if (key == null || key.equals(PreferencesUtils.getKey(TrackDetailActivity.this, R.string.recording_track_id_key))) {
-                recordingTrackId = PreferencesUtils.getLong(TrackDetailActivity.this, R.string.recording_track_id_key);
+            if (PreferencesUtils.isKey(TrackDetailActivity.this, R.string.recording_track_id_key, key)) {
+                recordingTrackId = PreferencesUtils.getRecordingTrackId(TrackDetailActivity.this);
             }
 
-            if (key == null || key.equals(PreferencesUtils.getKey(TrackDetailActivity.this, R.string.recording_track_paused_key))) {
+            if (PreferencesUtils.isKey(TrackDetailActivity.this, R.string.recording_track_paused_key, key)) {
                 recordingTrackPaused = PreferencesUtils.getBoolean(TrackDetailActivity.this, R.string.recording_track_paused_key, PreferencesUtils.RECORDING_TRACK_PAUSED_DEFAULT);
             }
 
-            if (key == null || key.equals(PreferencesUtils.getKey(TrackDetailActivity.this, R.string.trackdetail_show_on_lockscreen_while_recording_key))) {
+            if (PreferencesUtils.isKey(TrackDetailActivity.this, R.string.trackdetail_show_on_lockscreen_while_recording_key, key)) {
                 showOnLockScreen = PreferencesUtils.getBoolean(TrackDetailActivity.this, R.string.trackdetail_show_on_lockscreen_while_recording_key, PreferencesUtils.SHOW_TRACKDETAIL_WHILE_RECORDING_ON_LOCKSCREEN);
                 setLockscreenPolicy();
             }

src/main/java/de/dennisguse/opentracks/TrackListActivity.java
@@ -135,19 +135,16 @@ public class TrackListActivity extends AbstractListActivity implements ConfirmDe
             sharedPreferenceChangeListener = new OnSharedPreferenceChangeListener() {
         @Override
         public void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
-            if (key == null || key.equals(PreferencesUtils.getKey(TrackListActivity.this, R.string.stats_units_key))) {
+            if (PreferencesUtils.isKey(TrackListActivity.this, R.string.stats_units_key, key)) {
                 metricUnits = PreferencesUtils.isMetricUnits(TrackListActivity.this);
             }
-            if (key == null || key.equals(
-                    PreferencesUtils.getKey(TrackListActivity.this, R.string.recording_track_id_key))) {
-                recordingTrackId = PreferencesUtils.getLong(
-                        TrackListActivity.this, R.string.recording_track_id_key);
+            if (PreferencesUtils.isKey(TrackListActivity.this, R.string.recording_track_id_key, key)) {
+                recordingTrackId = PreferencesUtils.getRecordingTrackId(TrackListActivity.this);
                 if (key != null && PreferencesUtils.isRecording(recordingTrackId)) {
                     trackRecordingServiceConnection.startAndBind();
                 }
             }
-            if (key == null || key.equals(PreferencesUtils.getKey(
-                    TrackListActivity.this, R.string.recording_track_paused_key))) {
+            if (PreferencesUtils.isKey(TrackListActivity.this, R.string.recording_track_paused_key, key)) {
                 recordingTrackPaused = PreferencesUtils.getBoolean(TrackListActivity.this,
                         R.string.recording_track_paused_key,
                         PreferencesUtils.RECORDING_TRACK_PAUSED_DEFAULT);
@@ -226,7 +223,7 @@ public class TrackListActivity extends AbstractListActivity implements ConfirmDe
 
     private final OnClickListener recordListener = new OnClickListener() {
         public void onClick(View v) {
-            if (recordingTrackId == PreferencesUtils.RECORDING_TRACK_ID_DEFAULT) {
+            if (PreferencesUtils.isRecording(recordingTrackId)) {
                 // Not recording -> Recording
                 updateMenuItems(false, true);
                 startRecording();

src/main/java/de/dennisguse/opentracks/io/file/importer/KmzTrackImporter.java
@@ -101,7 +101,7 @@ public class KmzTrackImporter implements TrackImporter {
      * @param trackId the trackId
      */
     private void cleanImport(long trackId) {
-        if (trackId != PreferencesUtils.RECORDING_TRACK_ID_DEFAULT) {
+        if (!PreferencesUtils.isRecording(trackId)) {
             ContentProviderUtils contentProviderUtils = ContentProviderUtils.Factory.get(context);
             contentProviderUtils.deleteTrack(context, trackId);
         }

src/androidTest/java/de/dennisguse/opentracks/io/file/importer/GpxFileTrackImporterTest.java
@@ -27,7 +27,6 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
-import de.dennisguse.opentracks.R;
 import de.dennisguse.opentracks.content.Track;
 import de.dennisguse.opentracks.util.PreferencesUtils;
 
@@ -95,7 +94,7 @@ public class GpxFileTrackImporterTest extends AbstractTestFileTrackImporter {
         // A flush happens at the end
         when(contentProviderUtils.bulkInsertTrackPoint((Location[]) any(), eq(1), eq(TRACK_ID_0))).thenReturn(1);
         when(contentProviderUtils.getLastTrackPointId(TRACK_ID_0)).thenReturn(TRACK_POINT_ID_1);
-        when(contentProviderUtils.getTrack(PreferencesUtils.getLong(context, R.string.recording_track_id_key))).thenReturn(null);
+        when(contentProviderUtils.getTrack(PreferencesUtils.getRecordingTrackId(context))).thenReturn(null);
         ArgumentCaptor<Track> trackCaptor = ArgumentCaptor.forClass(Track.class);
         expectTrackUpdate(trackCaptor, true, TRACK_ID_0);
 
@@ -124,7 +123,7 @@ public class GpxFileTrackImporterTest extends AbstractTestFileTrackImporter {
         // A flush happens at the end
         when(contentProviderUtils.bulkInsertTrackPoint((Location[]) any(), eq(5), eq(TRACK_ID_0))).thenReturn(5);
         when(contentProviderUtils.getLastTrackPointId(TRACK_ID_0)).thenReturn(TRACK_POINT_ID_3);
-        when(contentProviderUtils.getTrack(PreferencesUtils.getLong(context, R.string.recording_track_id_key))).thenReturn(null);
+        when(contentProviderUtils.getTrack(PreferencesUtils.getRecordingTrackId(context))).thenReturn(null);
 
         ArgumentCaptor<Track> trackCaptor = ArgumentCaptor.forClass(Track.class);
         expectTrackUpdate(trackCaptor, true, TRACK_ID_0);
@@ -156,7 +155,7 @@ public class GpxFileTrackImporterTest extends AbstractTestFileTrackImporter {
         // A flush happens at the end
         when(contentProviderUtils.bulkInsertTrackPoint((Location[]) any(), eq(5), eq(TRACK_ID_0))).thenReturn(5);
         when(contentProviderUtils.getLastTrackPointId(TRACK_ID_0)).thenReturn(TRACK_POINT_ID_3);
-        when(contentProviderUtils.getTrack(PreferencesUtils.getLong(context, R.string.recording_track_id_key))).thenReturn(null);
+        when(contentProviderUtils.getTrack(PreferencesUtils.getRecordingTrackId(context))).thenReturn(null);
 
         ArgumentCaptor<Track> trackCaptor = ArgumentCaptor.forClass(Track.class);
         expectTrackUpdate(trackCaptor, true, TRACK_ID_0);
@@ -211,7 +210,7 @@ public class GpxFileTrackImporterTest extends AbstractTestFileTrackImporter {
         when(contentProviderUtils.bulkInsertTrackPoint((Location[]) any(), anyInt(), anyLong())).thenReturn(1);
         when(contentProviderUtils.getFirstTrackPointId(TRACK_ID_0)).thenReturn(TRACK_POINT_ID_0);
         when(contentProviderUtils.getLastTrackPointId(TRACK_ID_0)).thenReturn(TRACK_POINT_ID_0);
-        when(contentProviderUtils.getTrack(PreferencesUtils.getLong(context, R.string.recording_track_id_key))).thenReturn(null);
+        when(contentProviderUtils.getTrack(PreferencesUtils.getRecordingTrackId(context))).thenReturn(null);
         contentProviderUtils.deleteTrack(context, TRACK_ID_0);
 
         InputStream inputStream = new ByteArrayInputStream(xml.getBytes());

src/androidTest/java/de/dennisguse/opentracks/io/file/importer/KmlFileTrackImporterTest.java
@@ -27,7 +27,6 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
-import de.dennisguse.opentracks.R;
 import de.dennisguse.opentracks.content.Track;
 import de.dennisguse.opentracks.util.PreferencesUtils;
 
@@ -79,7 +78,7 @@ public class KmlFileTrackImporterTest extends AbstractTestFileTrackImporter {
         // A flush happens at the end
         when(contentProviderUtils.bulkInsertTrackPoint((Location[]) any(), eq(1), eq(TRACK_ID_0))).thenReturn(1);
         when(contentProviderUtils.getLastTrackPointId(TRACK_ID_0)).thenReturn(TRACK_POINT_ID_1);
-        when(contentProviderUtils.getTrack(PreferencesUtils.getLong(context, R.string.recording_track_id_key))).thenReturn(null);
+        when(contentProviderUtils.getTrack(PreferencesUtils.getRecordingTrackId(context))).thenReturn(null);
 
         ArgumentCaptor<Track> trackCaptor = ArgumentCaptor.forClass(Track.class);
         expectTrackUpdate(trackCaptor, true, TRACK_ID_0);
@@ -110,7 +109,7 @@ public class KmlFileTrackImporterTest extends AbstractTestFileTrackImporter {
         // A flush happens at the end
         when(contentProviderUtils.bulkInsertTrackPoint((Location[]) any(), eq(5), eq(TRACK_ID_0))).thenReturn(5);
         when(contentProviderUtils.getLastTrackPointId(TRACK_ID_0)).thenReturn(TRACK_POINT_ID_3);
-        when(contentProviderUtils.getTrack(PreferencesUtils.getLong(context, R.string.recording_track_id_key))).thenReturn(null);
+        when(contentProviderUtils.getTrack(PreferencesUtils.getRecordingTrackId(context))).thenReturn(null);
 
         ArgumentCaptor<Track> trackCaptor = ArgumentCaptor.forClass(Track.class);
         expectTrackUpdate(trackCaptor, true, TRACK_ID_0);

src/androidTest/java/de/dennisguse/opentracks/services/TrackRecordingServiceTest.java
@@ -163,7 +163,7 @@ public class TrackRecordingServiceTest {
         Assert.assertTrue(service.isRecording());
         Assert.assertEquals(id, service.getRecordingTrackId());
         mServiceRule.unbindService();
-        Assert.assertEquals(id, PreferencesUtils.getLong(context, R.string.recording_track_id_key));
+        Assert.assertEquals(id, PreferencesUtils.getRecordingTrackId(context));
 
         // Start the service in "resume" mode (simulates the on-reboot action).
 
@@ -297,7 +297,7 @@ public class TrackRecordingServiceTest {
         long newTrackId = service.startNewTrack();
         Assert.assertEquals(PreferencesUtils.RECORDING_TRACK_ID_DEFAULT, newTrackId);
 
-        Assert.assertEquals(trackId, PreferencesUtils.getLong(context, R.string.recording_track_id_key));
+        Assert.assertEquals(trackId, PreferencesUtils.getRecordingTrackId(context));
         Assert.assertEquals(trackId, service.getRecordingTrackId());
     }
 
@@ -310,7 +310,7 @@ public class TrackRecordingServiceTest {
         // Ending the current track when there is no recording should not result in any error.
         service.endCurrentTrack();
 
-        Assert.assertEquals(PreferencesUtils.RECORDING_TRACK_ID_DEFAULT, PreferencesUtils.getLong(context, R.string.recording_track_id_key));
+        Assert.assertFalse(PreferencesUtils.isRecording(context));
         Assert.assertEquals(PreferencesUtils.RECORDING_TRACK_ID_DEFAULT, service.getRecordingTrackId());
     }
 

src/androidTest/java/de/dennisguse/opentracks/services/TrackRecordingServiceTestLooper.java
@@ -225,7 +225,7 @@ public class TrackRecordingServiceTestLooper {
         Track track = providerUtils.getTrack(id);
         Assert.assertNotNull(track);
         Assert.assertEquals(id, track.getId());
-        Assert.assertEquals(id, PreferencesUtils.getLong(context, R.string.recording_track_id_key));
+        Assert.assertEquals(id, PreferencesUtils.getRecordingTrackId(context));
         Assert.assertEquals(id, service.getRecordingTrackId());
 
         // Insert a few points, markers and statistics.

src/main/java/de/dennisguse/opentracks/AbstractListActivity.java
@@ -65,7 +65,7 @@ public abstract class AbstractListActivity extends AbstractActivity implements C
         if (trackIds.length == 1 && trackIds[0] == -1L) {
             stopRecording = true;
         } else {
-            long recordingTrackId = PreferencesUtils.getLong(this, R.string.recording_track_id_key);
+            long recordingTrackId = PreferencesUtils.getRecordingTrackId(this);
             for (long trackId : trackIds) {
                 if (trackId == recordingTrackId) {
                     stopRecording = true;

attack-on-c-plus-plus/OpenTracks-Winter-2024-COMP-354

commit 0edcf55ed93499ee382e225e9f6f605d8ffac57e

Fixed issues introduced in 353c958ae060d9306355921efae8cbb235c87bb5 and 2a60aa8f59c5cf43d61ee36694180f894992bd1e.

src/main/java/de/dennisguse/opentracks/TrackDetailActivity.java
@@ -272,7 +272,7 @@ public class TrackDetailActivity extends AbstractListActivity implements ChooseA
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString(CURRENT_TAB_TAG_KEY, tabHost.getCurrentTabTag());
         if (photoUri != null) {

src/main/java/de/dennisguse/opentracks/TrackListActivity.java
@@ -221,7 +221,7 @@ public class TrackListActivity extends AbstractListActivity implements ConfirmDe
 
     private final OnClickListener recordListener = new OnClickListener() {
         public void onClick(View v) {
-            if (PreferencesUtils.isRecording(recordingTrackId)) {
+            if (!PreferencesUtils.isRecording(recordingTrackId)) {
                 // Not recording -> Recording
                 updateMenuItems(false, true);
                 startRecording();

src/main/java/de/dennisguse/opentracks/io/file/importer/KmzTrackImporter.java
@@ -101,7 +101,7 @@ public class KmzTrackImporter implements TrackImporter {
      * @param trackId the trackId
      */
     private void cleanImport(long trackId) {
-        if (!PreferencesUtils.isRecording(trackId)) {
+        if (PreferencesUtils.isRecording(trackId)) {
             ContentProviderUtils contentProviderUtils = ContentProviderUtils.Factory.get(context);
             contentProviderUtils.deleteTrack(context, trackId);
         }

src/main/res/values/preferences_internal.xml
@@ -20,5 +20,5 @@ limitations under the License.
     <string name="recording_track_id_key" translatable="false">recordingTrackId</string>
 
     <string name="recording_track_paused_key" translatable="false">recordingTrackPaused</string>
-    <bool name="recording_track_paused_default" translatable="false">false</bool>
+    <bool name="recording_track_paused_default" translatable="false">true</bool>
 </resources>