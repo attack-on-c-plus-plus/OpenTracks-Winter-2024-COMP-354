rgmf/OpenTracks

Added new option in Preferred rate setting: By Default. With By Default option it will use pace or speed depending on activity type. Fixes #151.

src/main/java/de/dennisguse/opentracks/fragments/ChartFragment.java
@@ -79,6 +79,7 @@ public class ChartFragment extends Fragment implements TrackDataListener {
     private boolean metricUnits = true;
     private boolean reportSpeed = true;
     private int recordingDistanceInterval;
+    private String category = "";
 
     // Modes of operation
     private boolean chartByDistance;
@@ -109,7 +110,7 @@ public class ChartFragment extends Fragment implements TrackDataListener {
                 }
             }
             if (PreferencesUtils.isKey(getContext(), R.string.stats_rate_key, key)) {
-                boolean speed = PreferencesUtils.isReportSpeed(getContext());
+                boolean speed = PreferencesUtils.isReportSpeed(getContext(), category);
                 if (reportSpeed != speed) {
                     reportSpeed = speed;
                     chartView.setReportSpeed(reportSpeed);
@@ -202,8 +203,10 @@ public class ChartFragment extends Fragment implements TrackDataListener {
         if (isResumed()) {
             if (track == null || track.getTripStatistics() == null) {
                 startTime = -1L;
+                category = "";
                 return;
             }
+            category = track.getCategory();
             startTime = track.getTripStatistics().getStartTime();
         }
     }

attack-on-c-plus-plus/OpenTracks-Winter-2024-COMP-354

src/main/java/de/dennisguse/opentracks/fragments/ChartFragment.java
@@ -75,6 +75,7 @@ public class ChartFragment extends Fragment implements TrackDataListener {
 
     //TODO Why is this needed?
     private int recordingDistanceInterval;
+    private String category = "";
 
     // Modes of operation
     private boolean chartByDistance;
@@ -100,7 +101,7 @@ public class ChartFragment extends Fragment implements TrackDataListener {
                 }
             }
             if (PreferencesUtils.isKey(getContext(), R.string.stats_rate_key, key)) {
-                boolean reportSpeed = PreferencesUtils.isReportSpeed(getContext());
+                boolean reportSpeed = PreferencesUtils.isReportSpeed(getContext(), category);
                 if (reportSpeed != chartView.getReportSpeed()) {
                     chartView.setReportSpeed(reportSpeed);
                     chartView.applyReportSpeed();
@@ -193,8 +194,11 @@ public class ChartFragment extends Fragment implements TrackDataListener {
         if (isResumed()) {
             if (track == null || track.getTrackStatistics() == null) {
                 startTime = -1L;
+                category = "";
                 return;
             }
+
+            category = track.getCategory();
             startTime = track.getTrackStatistics().getStartTime_ms();
         }
     }