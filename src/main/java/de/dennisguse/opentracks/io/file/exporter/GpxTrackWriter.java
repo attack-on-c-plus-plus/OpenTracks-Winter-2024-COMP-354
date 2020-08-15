pstorch/OpenTracks

Import/export track UUID (necessary for synchronization). If a track with a UUID is presented in OpenTracks, it cannot be imported again (enforced by database constraint). Fixes #307.

src/main/java/de/dennisguse/opentracks/io/file/exporter/GpxTrackWriter.java
@@ -34,6 +34,7 @@ import de.dennisguse.opentracks.util.StringUtils;
  * @author Sandor Dornbush
  */
 //TODO Can we export SensorData in GPX?
+//TODO Export waypoints
 public class GpxTrackWriter implements TrackWriter {
 
     private static final NumberFormat ELEVATION_FORMAT = NumberFormat.getInstance(Locale.US);
@@ -82,6 +83,7 @@ public class GpxTrackWriter implements TrackWriter {
             printWriter.println("xmlns=\"http://www.topografix.com/GPX/1/1\"");
             printWriter.println("xmlns:topografix=\"http://www.topografix.com/GPX/Private/TopoGrafix/0/1\"");
             printWriter.println("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"");
+            printWriter.println("xmlns:atom=\"http://www.w3.org/2005/Atom\"");
             printWriter.println("xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1"
                     + " http://www.topografix.com/GPX/1/1/gpx.xsd"
                     + " http://www.topografix.com/GPX/Private/TopoGrafix/0/1"
@@ -90,6 +92,7 @@ public class GpxTrackWriter implements TrackWriter {
             Track track = tracks[0];
             printWriter.println("<name>" + StringUtils.formatCData(track.getName()) + "</name>");
             printWriter.println("<desc>" + StringUtils.formatCData(track.getDescription()) + "</desc>");
+            printWriter.println("<atom:id>" + track.getUuid() + "</atom:id>");
             printWriter.println("</metadata>");
         }
     }
@@ -115,17 +118,15 @@ public class GpxTrackWriter implements TrackWriter {
     public void writeWaypoint(Waypoint waypoint) {
         if (printWriter != null) {
             Location location = waypoint.getLocation();
-            if (location != null) {
-                printWriter.println("<wpt " + formatLocation(location) + ">");
-                if (location.hasAltitude()) {
-                    printWriter.println("<ele>" + ELEVATION_FORMAT.format(location.getAltitude()) + "</ele>");
-                }
-                printWriter.println("<time>" + StringUtils.formatDateTimeIso8601(location.getTime()) + "</time>");
-                printWriter.println("<name>" + StringUtils.formatCData(waypoint.getName()) + "</name>");
-                printWriter.println("<desc>" + StringUtils.formatCData(waypoint.getDescription()) + "</desc>");
-                printWriter.println("<type>" + StringUtils.formatCData(waypoint.getCategory()) + "</type>");
-                printWriter.println("</wpt>");
+            printWriter.println("<wpt " + formatLocation(location) + ">");
+            if (location.hasAltitude()) {
+                printWriter.println("<ele>" + ELEVATION_FORMAT.format(location.getAltitude()) + "</ele>");
             }
+            printWriter.println("<time>" + StringUtils.formatDateTimeIso8601(location.getTime()) + "</time>");
+            printWriter.println("<name>" + StringUtils.formatCData(waypoint.getName()) + "</name>");
+            printWriter.println("<desc>" + StringUtils.formatCData(waypoint.getDescription()) + "</desc>");
+            printWriter.println("<type>" + StringUtils.formatCData(waypoint.getCategory()) + "</type>");
+            printWriter.println("</wpt>");
         }
     }
 
@@ -174,8 +175,7 @@ public class GpxTrackWriter implements TrackWriter {
             if (trackPoint.hasAltitude()) {
                 printWriter.println("<ele>" + ELEVATION_FORMAT.format(trackPoint.getAltitude()) + "</ele>");
             }
-            printWriter.println(
-                    "<time>" + StringUtils.formatDateTimeIso8601(trackPoint.getTime()) + "</time>");
+            printWriter.println("<time>" + StringUtils.formatDateTimeIso8601(trackPoint.getTime()) + "</time>");
             printWriter.println("<speed>" + trackPoint.getSpeed() + "</speed>");
             printWriter.println("</trkpt>");
         }

src/main/java/de/dennisguse/opentracks/io/file/importer/KmlFileTrackImporter.java
@@ -57,6 +57,7 @@ public class KmlFileTrackImporter extends AbstractFileTrackImporter {
     private static final String TAG_STYLE_URL = "styleUrl";
     private static final String TAG_VALUE = "value";
     private static final String TAG_WHEN = "when";
+    private static final String TAG_UUID = "atom:id";
 
     private static final String ATTRIBUTE_NAME = "name";
 
@@ -110,50 +111,70 @@ public class KmlFileTrackImporter extends AbstractFileTrackImporter {
 
     @Override
     public void endElement(String uri, String localName, String tag) throws SAXException {
-        //TODO Check if order is relevant (uses localname and tag); and convert to switch statement
-        if (tag.equals(TAG_KML)) {
-            onFileEnd();
-        } else if (tag.equals(TAG_PLACEMARK) || tag.equals(TAG_PHOTO_OVERLAY)) {
-            // Note that a track is contained in a Placemark, calling onWaypointend is save since waypointType is not set for a track.
-            onWaypointEnd();
-        } else if (localName.equals(TAG_COORDINATES)) {
-            onWaypointLocationEnd();
-        } else if (tag.equals(TAG_GX_MULTI_TRACK)) {
-            onTrackEnd();
-        } else if (tag.equals(TAG_GX_TRACK)) {
-            onTrackSegmentEnd();
-        } else if (tag.equals(TAG_GX_COORD)) {
-            onTrackPointEnd();
-        } else if (tag.equals(TAG_GX_VALUE)) {
-            onExtendedDataValueEnd();
-        } else if (tag.equals(TAG_NAME)) {
-            if (content != null) {
-                name = content.trim();
-            }
-        } else if (localName.equals(TAG_DESCRIPTION)) {
-            if (content != null) {
-                description = content.trim();
-            }
-        } else if (localName.equals(TAG_ICON)) {
-            if (content != null) {
-                icon = content.trim();
-            }
-        } else if (localName.equals(TAG_VALUE)) {
-            if (content != null) {
-                category = content.trim();
-            }
-        } else if (localName.equals(TAG_WHEN)) {
-            if (content != null) {
-                time = content.trim();
-            }
-        } else if (localName.equals(TAG_STYLE_URL)) {
-            if (content != null) {
-                waypointType = content.trim();
-            }
-        } else if (localName.equals(TAG_HREF)) {
-            if (content != null) {
-                photoUrl = content.trim();
-            }
+        switch (tag) {
+            case TAG_KML:
+                onFileEnd();
+                break;
+            case TAG_PLACEMARK:
+            case TAG_PHOTO_OVERLAY:
+                // Note that a track is contained in a Placemark, calling onWaypointend is save since waypointType is not set for a track.
+                onWaypointEnd();
+                break;
+            case TAG_COORDINATES:
+                onWaypointLocationEnd();
+                break;
+            case TAG_GX_MULTI_TRACK:
+                onTrackEnd();
+                break;
+            case TAG_GX_TRACK:
+                onTrackSegmentEnd();
+                break;
+            case TAG_GX_COORD:
+                onTrackPointEnd();
+                break;
+            case TAG_GX_VALUE:
+                onExtendedDataValueEnd();
+                break;
+            case TAG_NAME:
+                if (content != null) {
+                    name = content.trim();
+                }
+                break;
+            case TAG_UUID:
+                if (content != null) {
+                    uuid = content.trim();
+                }
+                break;
+            case TAG_DESCRIPTION:
+                if (content != null) {
+                    description = content.trim();
+                }
+                break;
+            case TAG_ICON:
+                if (content != null) {
+                    icon = content.trim();
+                }
+                break;
+            case TAG_VALUE:
+                if (content != null) {
+                    category = content.trim();
+                }
+                break;
+            case TAG_WHEN:
+                if (content != null) {
+                    time = content.trim();
+                }
+                break;
+            case TAG_STYLE_URL:
+                if (content != null) {
+                    waypointType = content.trim();
+                }
+                break;
+            case TAG_HREF:
+                if (content != null) {
+                    photoUrl = content.trim();
+                }
+                break;
         }
 
         // Reset element content

attack-on-c-plus-plus/OpenTracks-Winter-2024-COMP-354

src/main/java/de/dennisguse/opentracks/io/file/exporter/GpxTrackWriter.java
@@ -33,6 +33,7 @@ import de.dennisguse.opentracks.util.StringUtils;
  *
  * @author Sandor Dornbush
  */
+//TODO Export waypoints
 public class GpxTrackWriter implements TrackWriter {
 
     private static final NumberFormat ELEVATION_FORMAT = NumberFormat.getInstance(Locale.US);
@@ -93,10 +94,15 @@ public class GpxTrackWriter implements TrackWriter {
             printWriter.println("xmlns=\"http://www.topografix.com/GPX/1/1\"");
             printWriter.println("xmlns:topografix=\"http://www.topografix.com/GPX/Private/TopoGrafix/0/1\"");
             printWriter.println("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"");
+            printWriter.println("xmlns:atom=\"http://www.w3.org/2005/Atom\"");
+            printWriter.println("xmlns:opentracks=\"http://opentracksapp.com/xmlschemas/v1\"");
             printWriter.println("xmlns:gpxtpx=\"http://www.garmin.com/xmlschemes/TrackPointExtension/v2\"");
-            printWriter.println("xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd"
+            printWriter.println("xsi:schemaLocation=" +
+                    "\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd"
                     + " http://www.topografix.com/GPX/Private/TopoGrafix/0/1 http://www.topografix.com/GPX/Private/TopoGrafix/0/1/topografix.xsd"
-                    + " http://www.garmin.com/xmlschemas/TrackPointExtension/v2 https://www8.garmin.com/xmlschemas/TrackPointExtensionv2.xsd\">");
+                    + " http://www.garmin.com/xmlschemas/TrackPointExtension/v2 https://www8.garmin.com/xmlschemas/TrackPointExtensionv2.xsd"
+                    + " http://opentracksapp.com/xmlschemas/v1 http://opentracksapp.com/xmlschemas/OpenTracks_v1.xsd\">");
+
             printWriter.println("<metadata>");
 
             Track track = tracks[0];
@@ -127,17 +133,15 @@ public class GpxTrackWriter implements TrackWriter {
     public void writeWaypoint(Waypoint waypoint) {
         if (printWriter != null) {
             Location location = waypoint.getLocation();
-            if (location != null) {
-                printWriter.println("<wpt " + formatLocation(location) + ">");
-                if (location.hasAltitude()) {
-                    printWriter.println("<ele>" + ELEVATION_FORMAT.format(location.getAltitude()) + "</ele>");
-                }
-                printWriter.println("<time>" + StringUtils.formatDateTimeIso8601(location.getTime()) + "</time>");
-                printWriter.println("<name>" + StringUtils.formatCData(waypoint.getName()) + "</name>");
-                printWriter.println("<desc>" + StringUtils.formatCData(waypoint.getDescription()) + "</desc>");
-                printWriter.println("<type>" + StringUtils.formatCData(waypoint.getCategory()) + "</type>");
-                printWriter.println("</wpt>");
+            printWriter.println("<wpt " + formatLocation(location) + ">");
+            if (location.hasAltitude()) {
+                printWriter.println("<ele>" + ELEVATION_FORMAT.format(location.getAltitude()) + "</ele>");
             }
+            printWriter.println("<time>" + StringUtils.formatDateTimeIso8601(location.getTime()) + "</time>");
+            printWriter.println("<name>" + StringUtils.formatCData(waypoint.getName()) + "</name>");
+            printWriter.println("<desc>" + StringUtils.formatCData(waypoint.getDescription()) + "</desc>");
+            printWriter.println("<type>" + StringUtils.formatCData(waypoint.getCategory()) + "</type>");
+            printWriter.println("</wpt>");
         }
     }
 
@@ -158,7 +162,11 @@ public class GpxTrackWriter implements TrackWriter {
             printWriter.println("<name>" + StringUtils.formatCData(track.getName()) + "</name>");
             printWriter.println("<desc>" + StringUtils.formatCData(track.getDescription()) + "</desc>");
             printWriter.println("<type>" + StringUtils.formatCData(track.getCategory()) + "</type>");
-            printWriter.println("<extensions><topografix:color>c0c0c0</topografix:color></extensions>");
+
+            printWriter.println("<extensions>");
+            printWriter.println("<topografix:color>c0c0c0</topografix:color>");
+            printWriter.println("<opentracks:trackid>" + track.getUuid() + "</opentracks:trackid>");
+            printWriter.println("</extensions>");
         }
     }
 

src/main/java/de/dennisguse/opentracks/io/file/importer/KmlFileTrackImporter.java
@@ -57,6 +57,7 @@ public class KmlFileTrackImporter extends AbstractFileTrackImporter {
     private static final String TAG_STYLE_URL = "styleUrl";
     private static final String TAG_VALUE = "value";
     private static final String TAG_WHEN = "when";
+    private static final String TAG_UUID = "opentracks:trackid";
 
     private static final String ATTRIBUTE_NAME = "name";
 
@@ -68,23 +69,17 @@ public class KmlFileTrackImporter extends AbstractFileTrackImporter {
     private ArrayList<Float> heartRateList;
     private ArrayList<Float> powerList;
 
-    /**
-     * @param context       the context
-     * @param importTrackId track id to import to. This should not be -1L so that images in the kmz file can be imported.
-     */
-    @Deprecated //TODO Do not pass importTrackId here; get it from somewhere else
-    public KmlFileTrackImporter(Context context, long importTrackId) {
-        this(context, importTrackId, new ContentProviderUtils(context));
+    public KmlFileTrackImporter(Context context) {
+        this(context, new ContentProviderUtils(context));
     }
 
     @VisibleForTesting
-    KmlFileTrackImporter(Context context, long importTrackId, ContentProviderUtils contentProviderUtils) {
-        super(context, importTrackId, contentProviderUtils);
+    KmlFileTrackImporter(Context context, ContentProviderUtils contentProviderUtils) {
+        super(context, contentProviderUtils);
     }
 
     @Override
-    public void startElement(String uri, String localName, String tag, Attributes attributes)
-            throws SAXException {
+    public void startElement(String uri, String localName, String tag, Attributes attributes) throws SAXException {
         switch (tag) {
             case TAG_PLACEMARK:
             case TAG_PHOTO_OVERLAY:
@@ -109,50 +104,70 @@ public class KmlFileTrackImporter extends AbstractFileTrackImporter {
 
     @Override
     public void endElement(String uri, String localName, String tag) throws SAXException {
-        //TODO Check if order is relevant (uses localname and tag); and convert to switch statement
-        if (tag.equals(TAG_KML)) {
-            onFileEnd();
-        } else if (tag.equals(TAG_PLACEMARK) || tag.equals(TAG_PHOTO_OVERLAY)) {
-            // Note that a track is contained in a Placemark, calling onWaypointend is save since waypointType is not set for a track.
-            onWaypointEnd();
-        } else if (localName.equals(TAG_COORDINATES)) {
-            onWaypointLocationEnd();
-        } else if (tag.equals(TAG_GX_MULTI_TRACK)) {
-            onTrackEnd();
-        } else if (tag.equals(TAG_GX_TRACK)) {
-            onTrackSegmentEnd();
-        } else if (tag.equals(TAG_GX_COORD)) {
-            onTrackPointEnd();
-        } else if (tag.equals(TAG_GX_VALUE)) {
-            onExtendedDataValueEnd();
-        } else if (tag.equals(TAG_NAME)) {
-            if (content != null) {
-                name = content.trim();
-            }
-        } else if (localName.equals(TAG_DESCRIPTION)) {
-            if (content != null) {
-                description = content.trim();
-            }
-        } else if (localName.equals(TAG_ICON)) {
-            if (content != null) {
-                icon = content.trim();
-            }
-        } else if (localName.equals(TAG_VALUE)) {
-            if (content != null) {
-                category = content.trim();
-            }
-        } else if (localName.equals(TAG_WHEN)) {
-            if (content != null) {
-                time = content.trim();
-            }
-        } else if (localName.equals(TAG_STYLE_URL)) {
-            if (content != null) {
-                waypointType = content.trim();
-            }
-        } else if (localName.equals(TAG_HREF)) {
-            if (content != null) {
-                photoUrl = content.trim();
-            }
+        switch (tag) {
+            case TAG_KML:
+                onFileEnd();
+                break;
+            case TAG_PLACEMARK:
+            case TAG_PHOTO_OVERLAY:
+                // Note that a track is contained in a Placemark, calling onWaypointend is save since waypointType is not set for a track.
+                onWaypointEnd();
+                break;
+            case TAG_COORDINATES:
+                onWaypointLocationEnd();
+                break;
+            case TAG_GX_MULTI_TRACK:
+                onTrackEnd();
+                break;
+            case TAG_GX_TRACK:
+                onTrackSegmentEnd();
+                break;
+            case TAG_GX_COORD:
+                onTrackPointEnd();
+                break;
+            case TAG_GX_VALUE:
+                onExtendedDataValueEnd();
+                break;
+            case TAG_NAME:
+                if (content != null) {
+                    name = content.trim();
+                }
+                break;
+            case TAG_UUID:
+                if (content != null) {
+                    uuid = content.trim();
+                }
+                break;
+            case TAG_DESCRIPTION:
+                if (content != null) {
+                    description = content.trim();
+                }
+                break;
+            case TAG_ICON:
+                if (content != null) {
+                    icon = content.trim();
+                }
+                break;
+            case TAG_VALUE:
+                if (content != null) {
+                    category = content.trim();
+                }
+                break;
+            case TAG_WHEN:
+                if (content != null) {
+                    time = content.trim();
+                }
+                break;
+            case TAG_STYLE_URL:
+                if (content != null) {
+                    waypointType = content.trim();
+                }
+                break;
+            case TAG_HREF:
+                if (content != null) {
+                    photoUrl = content.trim();
+                }
+                break;
         }
 
         // Reset element content