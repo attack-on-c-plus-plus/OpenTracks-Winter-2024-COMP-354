eiabea/OpenTracks

Use distance from cycling speed sensors. Fixes #505.

src/main/java/de/dennisguse/opentracks/content/provider/ContentProviderUtils.java
@@ -555,6 +555,9 @@ public class ContentProviderUtils {
         if (!cursor.isNull(indexes.sensorCadenceIndex)) {
             trackPoint.setCyclingCadence_rpm(cursor.getFloat(indexes.sensorCadenceIndex));
         }
+        if (!cursor.isNull(indexes.sensorDistanceIndex)) {
+            trackPoint.setDistance(cursor.getFloat(indexes.sensorDistanceIndex));
+        }
         if (!cursor.isNull(indexes.sensorPowerIndex)) {
             trackPoint.setPower(cursor.getFloat(indexes.sensorPowerIndex));
         }
@@ -724,6 +727,9 @@ public class ContentProviderUtils {
         }
         if (trackPoint.hasCyclingCadence()) {
             values.put(TrackPointsColumns.SENSOR_CADENCE, trackPoint.getCyclingCadence_rpm());
+        }
+        if (trackPoint.hasDistance()) {
+            values.put(TrackPointsColumns.SENSOR_DISTANCE, trackPoint.getDistance());
         }
         if (trackPoint.hasPower()) {
             values.put(TrackPointsColumns.SENSOR_POWER, trackPoint.getPower());

src/main/java/de/dennisguse/opentracks/io/file/exporter/KMLTrackExporter.java
@@ -60,6 +60,7 @@ public class KMLTrackExporter implements TrackExporter {
     private static final String SCHEMA_ID = "schema";
 
     public static final String EXTENDED_DATA_TYPE_SPEED = "speed";
+    public static final String EXTENDED_DATA_TYPE_DISTANCE = "distance";
     public static final String EXTENDED_DATA_TYPE_CADENCE = "cadence";
     public static final String EXTENDED_DATA_TYPE_HEART_RATE = "heart_rate";
     public static final String EXTENDED_DATA_TYPE_POWER = "power";
@@ -80,6 +81,7 @@ public class KMLTrackExporter implements TrackExporter {
 
     private PrintWriter printWriter;
     private final List<Float> speedList = new ArrayList<>();
+    private final List<Float> distanceList = new ArrayList<>();
     private final List<Float> powerList = new ArrayList<>();
     private final List<Float> cadenceList = new ArrayList<>();
     private final List<Float> heartRateList = new ArrayList<>();
@@ -355,6 +357,7 @@ public class KMLTrackExporter implements TrackExporter {
         if (printWriter != null) {
             printWriter.println("<gx:Track>");
             speedList.clear();
+            distanceList.clear();
             powerList.clear();
             cadenceList.clear();
             heartRateList.clear();
@@ -372,6 +375,9 @@ public class KMLTrackExporter implements TrackExporter {
                 writeSimpleArrayData(speedList, EXTENDED_DATA_TYPE_SPEED);
             }
             if (exportSensorData) {
+                if (distanceList.stream().anyMatch(Objects::nonNull)) {
+                    writeSimpleArrayData(distanceList, EXTENDED_DATA_TYPE_DISTANCE);
+                }
                 if (powerList.stream().anyMatch(Objects::nonNull)) {
                     writeSimpleArrayData(powerList, EXTENDED_DATA_TYPE_POWER);
                 }
@@ -409,6 +415,7 @@ public class KMLTrackExporter implements TrackExporter {
             speedList.add(trackPoint.hasSpeed() ? trackPoint.getSpeed() : null);
 
             if (exportSensorData) {
+                distanceList.add(trackPoint.hasDistance() ? trackPoint.getDistance() : null);
                 heartRateList.add(trackPoint.hasHeartRate() ? trackPoint.getHeartRate_bpm() : null);
                 cadenceList.add(trackPoint.hasCyclingCadence() ? trackPoint.getCyclingCadence_rpm() : null);
                 powerList.add(trackPoint.hasPower() ? trackPoint.getPower() : null);

src/main/java/de/dennisguse/opentracks/io/file/importer/AbstractFileTrackImporter.java
@@ -88,6 +88,7 @@ abstract class AbstractFileTrackImporter extends DefaultHandler implements Track
     protected String uuid;
     protected String gain;
     protected String loss;
+    protected String distance;
 
     // The current track data
     private TrackData trackData;
@@ -134,9 +135,6 @@ abstract class AbstractFileTrackImporter extends DefaultHandler implements Track
         }
     }
 
-    /**
-     * On file end.
-     */
     protected void onFileEnd() {
         // Add markers to the last imported track
         int size = trackIds.size();
@@ -491,6 +489,13 @@ abstract class AbstractFileTrackImporter extends DefaultHandler implements Track
                 trackPoint.setElevationLoss(Float.parseFloat(loss));
             } catch (Exception e) {
                 throw new SAXException(createErrorMessage(String.format(Locale.US, "Unable to parse elevation loss: %s", loss)), e);
+            }
+        }
+        if (distance != null) {
+            try {
+                trackPoint.setDistance(Float.parseFloat(distance));
+            } catch (Exception e) {
+                throw new SAXException(createErrorMessage(String.format(Locale.US, "Unable to parse distance: %s", distance)), e);
             }
         }
 

attack-on-c-plus-plus/OpenTracks-Winter-2024-COMP-354

src/main/java/de/dennisguse/opentracks/content/provider/ContentProviderUtils.java
@@ -556,6 +556,9 @@ public class ContentProviderUtils {
         if (!cursor.isNull(indexes.sensorCadenceIndex)) {
             trackPoint.setCyclingCadence_rpm(cursor.getFloat(indexes.sensorCadenceIndex));
         }
+        if (!cursor.isNull(indexes.sensorDistanceIndex)) {
+            trackPoint.setSensorDistance(cursor.getFloat(indexes.sensorDistanceIndex));
+        }
         if (!cursor.isNull(indexes.sensorPowerIndex)) {
             trackPoint.setPower(cursor.getFloat(indexes.sensorPowerIndex));
         }
@@ -707,6 +710,9 @@ public class ContentProviderUtils {
         }
         if (trackPoint.hasCyclingCadence()) {
             values.put(TrackPointsColumns.SENSOR_CADENCE, trackPoint.getCyclingCadence_rpm());
+        }
+        if (trackPoint.hasSensorDistance()) {
+            values.put(TrackPointsColumns.SENSOR_DISTANCE, trackPoint.getSensorDistance());
         }
         if (trackPoint.hasPower()) {
             values.put(TrackPointsColumns.SENSOR_POWER, trackPoint.getPower());

src/main/java/de/dennisguse/opentracks/io/file/exporter/KMLTrackExporter.java
@@ -57,6 +57,7 @@ public class KMLTrackExporter implements TrackExporter {
     private static final String SCHEMA_ID = "schema";
 
     public static final String EXTENDED_DATA_TYPE_SPEED = "speed";
+    public static final String EXTENDED_DATA_TYPE_DISTANCE = "distance";
     public static final String EXTENDED_DATA_TYPE_CADENCE = "cadence";
     public static final String EXTENDED_DATA_TYPE_HEART_RATE = "heart_rate";
     public static final String EXTENDED_DATA_TYPE_POWER = "power";
@@ -74,6 +75,7 @@ public class KMLTrackExporter implements TrackExporter {
 
     private PrintWriter printWriter;
     private final List<Float> speedList = new ArrayList<>();
+    private final List<Float> distanceList = new ArrayList<>();
     private final List<Float> powerList = new ArrayList<>();
     private final List<Float> cadenceList = new ArrayList<>();
     private final List<Float> heartRateList = new ArrayList<>();
@@ -338,6 +340,7 @@ public class KMLTrackExporter implements TrackExporter {
         if (printWriter != null) {
             printWriter.println("<gx:Track>");
             speedList.clear();
+            distanceList.clear();
             powerList.clear();
             cadenceList.clear();
             heartRateList.clear();
@@ -355,6 +358,9 @@ public class KMLTrackExporter implements TrackExporter {
                 writeSimpleArrayData(speedList, EXTENDED_DATA_TYPE_SPEED);
             }
             if (exportSensorData) {
+                if (distanceList.stream().anyMatch(Objects::nonNull)) {
+                    writeSimpleArrayData(distanceList, EXTENDED_DATA_TYPE_DISTANCE);
+                }
                 if (powerList.stream().anyMatch(Objects::nonNull)) {
                     writeSimpleArrayData(powerList, EXTENDED_DATA_TYPE_POWER);
                 }
@@ -392,6 +398,7 @@ public class KMLTrackExporter implements TrackExporter {
             speedList.add(trackPoint.hasSpeed() ? trackPoint.getSpeed() : null);
 
             if (exportSensorData) {
+                distanceList.add(trackPoint.hasSensorDistance() ? trackPoint.getSensorDistance() : null);
                 heartRateList.add(trackPoint.hasHeartRate() ? trackPoint.getHeartRate_bpm() : null);
                 cadenceList.add(trackPoint.hasCyclingCadence() ? trackPoint.getCyclingCadence_rpm() : null);
                 powerList.add(trackPoint.hasPower() ? trackPoint.getPower() : null);

src/main/java/de/dennisguse/opentracks/io/file/importer/AbstractFileTrackImporter.java
@@ -91,6 +91,7 @@ abstract class AbstractFileTrackImporter extends DefaultHandler implements Track
     protected String uuid;
     protected String gain;
     protected String loss;
+    protected String distance;
 
     // The current track data
     private TrackData trackData;
@@ -141,9 +142,6 @@ abstract class AbstractFileTrackImporter extends DefaultHandler implements Track
         }
     }
 
-    /**
-     * On file end.
-     */
     protected void onFileEnd() {
         // Add markers to the last imported track
         int size = trackIds.size();
@@ -376,7 +374,7 @@ abstract class AbstractFileTrackImporter extends DefaultHandler implements Track
                      * GPS points tend to have some inherent imprecision, speed and bearing will likely be off, so the statistics for things like max speed will also be off.
                      */
                     if (trackPoint.hasLocation() && trackData.lastLocationInCurrentSegment.hasLocation()) {
-                        float speed = trackData.lastLocationInCurrentSegment.distanceTo(trackPoint) / timeDifference.toMillis();
+                        float speed = trackData.lastLocationInCurrentSegment.distanceToPrevious(trackPoint) / timeDifference.toMillis();
                         trackPoint.setSpeed(speed);
                     }
                 }
@@ -385,7 +383,7 @@ abstract class AbstractFileTrackImporter extends DefaultHandler implements Track
                 trackPoint.setBearing(trackData.lastLocationInCurrentSegment.bearingTo(trackPoint));
 
                 long maxRecordingDistance = PreferencesUtils.getMaxRecordingDistance(sharedPreferences, context); //TODO Should only be read once!
-                double distanceToLastTrackLocation = trackPoint.distanceTo(trackData.lastLocationInCurrentSegment);
+                double distanceToLastTrackLocation = trackPoint.distanceToPrevious(trackData.lastLocationInCurrentSegment);
                 if (distanceToLastTrackLocation > maxRecordingDistance) {
                     trackPoint.setType(TrackPoint.Type.SEGMENT_START_AUTOMATIC);
                 }
@@ -503,6 +501,13 @@ abstract class AbstractFileTrackImporter extends DefaultHandler implements Track
                 trackPoint.setElevationLoss(Float.parseFloat(loss));
             } catch (Exception e) {
                 throw new ParsingException(createErrorMessage(String.format(Locale.US, "Unable to parse elevation loss: %s", loss)), e);
+            }
+        }
+        if (distance != null) {
+            try {
+                trackPoint.setSensorDistance(Float.parseFloat(distance));
+            } catch (Exception e) {
+                throw new ParsingException(createErrorMessage(String.format(Locale.US, "Unable to parse distance: %s", distance)), e);
             }
         }
 