d31f42cd6e7429db9c08b195aa35fb852adac0bf
carbo18 <carbo18@gmail.com>
2024-03-24 00:45:57 -0400

    Add RunLiftStatisticsTest cases

--- src/androidTest/java/de/dennisguse/opentracks/ui/runlift/RunLiftStatisticsTest.java
+++ src/androidTest/java/de/dennisguse/opentracks/ui/runlift/RunLiftStatisticsTest.java
@@ -5,11 +5,16 @@ import android.content.Context;
 import androidx.test.core.app.ApplicationProvider;
 
 import de.dennisguse.opentracks.data.ContentProviderUtils;
+import de.dennisguse.opentracks.data.models.Track;
 import de.dennisguse.opentracks.ui.intervals.IntervalStatisticsTest;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnit4.class)
 public class RunLiftStatisticsTest {
@@ -24,5 +29,52 @@ public class RunLiftStatisticsTest {
         contentProviderUtils = new ContentProviderUtils(context);
     }
 
+    @Test
+    public void testLiftOnlyTrackPoints() {
+        Track dummyTrack = new Track();
+        dummyTrack.setId(new Track.Id(System.currentTimeMillis()));
+        assertTrue(true);
+    }
 
+    @Test
+    public void testRunOnlyTrackPoints() {
+        Track dummyTrack = new Track();
+        dummyTrack.setId(new Track.Id(System.currentTimeMillis()));
+        assertTrue(true);
+    }
+
+    @Test
+    public void testLiftWithWaitTrackPoints() {
+        Track dummyTrack = new Track();
+        dummyTrack.setId(new Track.Id(System.currentTimeMillis()));
+        assertTrue(true);
+    }
+
+    @Test
+    public void testLiftAndRunTrackPoints() {
+        Track dummyTrack = new Track();
+        dummyTrack.setId(new Track.Id(System.currentTimeMillis()));
+        assertTrue(true);
+    }
+
+    @Test
+    public void testLiftAndRunWithWaitTrackPoints() {
+        Track dummyTrack = new Track();
+        dummyTrack.setId(new Track.Id(System.currentTimeMillis()));
+        assertTrue(true);
+    }
+
+    @Test
+    public void testLiftAndRunAndLiftTrackPoints() {
+        Track dummyTrack = new Track();
+        dummyTrack.setId(new Track.Id(System.currentTimeMillis()));
+        assertTrue(true);
+    }
+
+    @Test
+    public void testLiftAndRunAndLiftWithWaitTimesTrackPoints() {
+        Track dummyTrack = new Track();
+        dummyTrack.setId(new Track.Id(System.currentTimeMillis()));
+        assertTrue(true);
+    }
 }
